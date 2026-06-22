20e337b5e86f02f643f3eb913b3c94c046b0819f
Joselyn Sigcha <ja.sigchaq@uea.edu.ec>
2026-06-21 19:24:49 -0500

    CPE01 – Clase Horario Consulta de la carpeta de Modelo del sistema agenda de turnos de pacientes de una clínica 🏥

---
+++ Modelo/HorarioConsulta.cs
@@ -0,0 +1,12 @@
+namespace AgendaClinica.Modelo
+{
+    // Estructura que almacena la fecha y hora de una consulta médica
+    struct HorarioConsulta
+    {
+        // Almacena el día de la consulta
+        public string Dia { get; set; }
+        
+        // Almacena la hora de la consulta
+        public string Hora { get; set; }
+    }
+}
